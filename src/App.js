eaf025bed90ad00449ca75e32dd868f67d413758
rakib-netmow <rakib.netmow@gmail.com>
2024-01-02 17:52:08 +0600

    handler added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newData = new FormData();
+    newData.append("file", file);
+    console.log("submited", newData.get("file"));
+  };
   return (
     <div className="App">
-      <form>
-        <input type="file" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <br />
         <br />
         <input type="submit" value="import" />
