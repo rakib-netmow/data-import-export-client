b1f9a71438ca71f1947ef9d2f330a222cccb2ae9
rakib-netmow <rakib.netmow@gmail.com>
2024-01-09 14:12:51 +0600

    all required filed added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import axios from "axios";
 
 function App() {
   const [file, setFile] = useState(null);
+  const [csvFields, setCsvFields] = useState([]);
+  const [desireFields, setDesireFields] = useState([]);
+  const [data, setData] = useState({});
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newData = new FormData();
@@ -11,7 +14,13 @@ function App() {
     // console.log("submited", newData.get("file"));
 
     const res = await axios.post("http://localhost:5000/v1/import", newData);
+    setCsvFields(res?.data);
+    const res2 = await axios.get(
+      "http://localhost:5000/v1/all-guardian-fields"
+    );
+    setDesireFields(res2?.data);
     console.log(res);
+    console.log(res2);
   };
   return (
     <div className="App">
@@ -29,14 +38,28 @@ function App() {
       <br />
 
       <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          name="file"
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <br />
-        <br />
-        <input type="submit" value="import" />
+        {csvFields?.length > 0 &&
+          csvFields?.map((f, i) => (
+            <div key={i}>
+              <label>{f}</label>
+              <select
+                onChange={(e) => {
+                  const value = e.target.value;
+                  const newObject = Object.fromEntries([[value, csvFields[i]]]);
+                  console.log(newObject);
+                  const newField = {
+                    ...data,
+                    ...newObject,
+                  };
+                  setData(newField);
+                  console.log(data);
+                }}
+              >
+                {desireFields?.length > 0 &&
+                  desireFields?.map((d, i) => <option key={i}>{d}</option>)}
+              </select>
+            </div>
+          ))}
       </form>
     </div>
   );
